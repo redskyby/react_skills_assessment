801c737e67ac188dd2cf3e3e54a2891e86e935bf
Docenko Pasha <pashadocenko@gmail.com>
2023-10-31 02:12:40 +0300

    add some styles.

--- src/components/SearchForm.tsx
+++ src/components/SearchForm.tsx
@@ -20,7 +20,7 @@ const SearchForm = () => {
         : [];
 
     return (
-        <Form>
+        <Form style={{position: "relative"}}>
             <FormGroup controlId={'searchForm'}>
                 <Form.Label>Поиск монеты по названию</Form.Label>
                 <Form.Control
@@ -30,9 +30,9 @@ const SearchForm = () => {
                     onChange={(e) => setCoin(e.target.value)}
                 />
             </FormGroup>
-            <ListGroup>
+            <ListGroup style={{position: "absolute" , top : '110%' , left : '0'}}>
                 {filteredCoins.map((coin) => (
-                    <ListGroup.Item
+                    <ListGroup.Item style={{cursor : 'pointer'}}
                         key={coin.id}
                         onClick={e =>  history(COIN_ROUTE + `/${coin.id}`)}
                     >{coin.name}</ListGroup.Item>
