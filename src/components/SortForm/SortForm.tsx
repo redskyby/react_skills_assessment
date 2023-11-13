c9a77344b6f1462f74beeafbeb6c659af734a064
Docenko Pasha <pashadocenko@gmail.com>
2023-11-13 13:14:41 +0300

    SortForm.tsx was started to rewrite.

--- src/components/SortForm/SortForm.tsx
+++ src/components/SortForm/SortForm.tsx
@@ -1,19 +1,32 @@
 import React from 'react';
-import { Form } from 'react-bootstrap';
+// import style from './SortForm.module.scss';
 
 const SortForm = ({ setSort }: { setSort: React.Dispatch<React.SetStateAction<string>> }) => {
     return (
-        <Form.Select
-            onChange={(e: React.ChangeEvent<HTMLSelectElement>) => {
-                setSort(e.target.value);
-            }}
-            className={'mt-3'}
-        >
-            <option>Сортировать по:</option>
-            <option value="1">цене</option>
-            <option value="2">рыночной капитализации</option>
-            <option value="3">изменению за 24ч</option>
-        </Form.Select>
+        // <Form.Select
+        //     onChange={(e: React.ChangeEvent<HTMLSelectElement>) => {
+        //         setSort(e.target.value);
+        //     }}
+        //     className={'mt-3'}
+        // >
+        //     <option>Сортировать по:</option>
+        //     <option value="1">цене</option>
+        //     <option value="2">рыночной капитализации</option>
+        //     <option value="3">изменению за 24ч</option>
+        // </Form.Select>
+        <form>
+            <select
+                name="sort"
+                onChange={(e: React.ChangeEvent<HTMLSelectElement>) => {
+                    setSort(e.target.value);
+                }}
+            >
+                <option value="">Сортировать по:</option>
+                <option value="1">цене</option>
+                <option value="2">рыночной капитализации</option>
+                <option value="3">изменению за 24ч</option>
+            </select>
+        </form>
     );
 };
 
