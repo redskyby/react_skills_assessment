f8aa4276c1db26b31a4b381eaf029111b00db7e6
Docenko Pasha <pashadocenko@gmail.com>
2023-11-16 01:51:40 +0300

    CoinPage.tsx was rewritten.

--- src/page/CoinPage.tsx
+++ src/page/CoinPage.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { NavLink, useParams } from 'react-router-dom';
 import { useGetOneCoinQuery, CoinOne } from '../redux/query/CoinQuery';
-import { Button, Container, Image, Row } from 'react-bootstrap';
 import { RingLoader } from 'react-spinners';
 import secondNumberAfterDot from '../utils/SecondNumberAfterDot';
 import icon from '../utils/imgIcon/favicon.png';
@@ -34,48 +33,60 @@ const CoinPage = () => {
                     <RingLoader color={'#36d7b7'} size={'100px'} />
                 </div>
             ) : data ? (
-                <Container>
-                    <Row className={'text-center'}>
+                <div className={style.page_of_coin}>
+                    <div className={style.page_of_coin_main_title}>
                         <h1>{data.data.id}</h1>
-                    </Row>
-                    <table>
+                    </div>
+                    <table className={style.page_of_coin_table}>
                         <thead>
                             <tr>
-                                <th className={'align-middle'}>Изображение монеты</th>
-                                <th className={'align-middle'}>Название монеты</th>
-                                <th className={'align-middle'}>Символ монеты</th>
-                                <th className={'align-middle'}>rank</th>
-                                <th className={'align-middle'}>Supply</th>
-                                <th className={'align-middle'}>Цена в USD</th>
-                                <th className={'align-middle'}>Рыночная капитализация в USD</th>
-                                <th className={'align-middle'}>maxSupply</th>
+                                <th className={style.page_of_coin_table_text_center}>Изображение монеты</th>
+                                <th className={style.page_of_coin_table_text_center}>Название монеты</th>
+                                <th className={style.page_of_coin_table_text_center}>Символ монеты</th>
+                                <th className={style.page_of_coin_table_text_center}>rank</th>
+                                <th className={style.page_of_coin_table_text_center}>Supply</th>
+                                <th className={style.page_of_coin_table_text_center}>Цена в USD</th>
+                                <th className={style.page_of_coin_table_text_center}>Рыночная капитализация в USD</th>
+                                <th className={style.page_of_coin_table_text_center}>maxSupply</th>
                             </tr>
                         </thead>
                         <tbody>
                             <tr>
-                                <td className={'align-middle'}>
-                                    <Image src={icon} />
+                                <td className={style.page_of_coin_table_text_center}>
+                                    <img src={icon} alt="Main logo" />
                                 </td>
-                                <td className={'align-middle'}>{data.data.id}</td>
-                                <td className={'align-middle'}>{data.data.symbol}</td>
-                                <td className={'align-middle'}>{data.data.rank}</td>
-                                <td className={'align-middle'}>{secondNumberAfterDot(data.data.supply)}</td>
-                                <td className={'align-middle'}>{secondNumberAfterDot(data.data.priceUsd)}</td>
-                                <td className={'align-middle'}>{secondNumberAfterDot(data.data.marketCapUsd)}</td>
-                                <td className={'align-middle'}>{secondNumberAfterDot(data.data.maxSupply)}</td>
-                                <td className={'align-middle'}>
-                                    <Button type={'button'} onClick={(e) => addInSuitCase(e, data)}>
+                                <td className={style.page_of_coin_table_text_center}>{data.data.id}</td>
+                                <td className={style.page_of_coin_table_text_center}>{data.data.symbol}</td>
+                                <td className={style.page_of_coin_table_text_center}>{data.data.rank}</td>
+                                <td className={style.page_of_coin_table_text_center}>
+                                    {secondNumberAfterDot(data.data.supply)}
+                                </td>
+                                <td className={style.page_of_coin_table_text_center}>
+                                    {secondNumberAfterDot(data.data.priceUsd)}
+                                </td>
+                                <td className={style.page_of_coin_table_text_center}>
+                                    {secondNumberAfterDot(data.data.marketCapUsd)}
+                                </td>
+                                <td className={style.page_of_coin_table_text_center}>
+                                    {secondNumberAfterDot(data.data.maxSupply)}
+                                </td>
+                                <td className={style.page_of_coin_table_text_center}>
+                                    <button
+                                        className={style.button}
+                                        type={'button'}
+                                        onClick={(e) => addInSuitCase(e, data)}
+                                    >
                                         Добавить в портфель
-                                    </Button>
+                                    </button>
                                 </td>
                             </tr>
                         </tbody>
                     </table>
-                    <Row>
+                    <div>
                         <NavLink to={SHOP_ROUTE}>Вернуться на главную страницу</NavLink>
-                    </Row>
+                    </div>
                     <CoinWasAdded />
-                </Container>
+                </div>
             ) : null}
         </div>
     );
